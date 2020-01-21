a8883ddf35c9a7cd4a8348e21e683f284db229d9
Cyril Panicker <cpr@financial.com>
2020-01-21 16:58:11 +0530

    changes

---
+++ ci-callback.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+const owner = process.argv[2];
+const repo = process.argv[3];
+const sha = process.argv[4];
+
+(async ()=>{
+    await axios.get(`http://localhost:8080/ci-callback?branch=${branch}&owner=${owner}&repo=${repo}&sha=${sha}`);
+})();
